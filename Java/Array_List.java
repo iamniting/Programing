69882f228a54f1c79ddb9da1a6ea31421b892b7d
Nitin Goyal <iamniting@gmail.com>
2017-06-01 17:45:41 +0530

    Create Array_List.java

---
+++ Java/Array_List.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Array_List
+{
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList <Integer> list= new ArrayList <Integer>();
+        Iterator itr;
+        
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++)
+        {
+            int a = sc.nextInt();
+            list.add(a);
+        }
+        
+        
+        for( itr = list.iterator(); itr.hasNext(); )
+        {
+            int a = (Integer)itr.next();
+            if(a > 0)
+            System.out.println(a);
+        }
+        
+        
+        list.clear();
+        itr = list.iterator();
+        if(itr.hasNext())
+            System.out.println("Not Empty");
+    }
+}
