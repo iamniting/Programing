ff403a3728b35f2a16462c99de4ad6401f55a2e1
Nitin Goyal <iamniting@gmail.com>
2017-06-01 17:24:14 +0530

    Create Input.java

---
+++ Java/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Input
+{
+	public static void main(String []args)
+	{
+		//Scanner reader = new Scanner(System.in);
+		System.out.println("Enter any integer");
+		//int n = reader.nextInt();
+		int n = new Scanner(System.in).nextInt();
+		System.out.println(n);
+	}
+}
