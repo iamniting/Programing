256faaafe6af219cd8679b2ee3d3c6f3f77df3fd
Nitin Goyal <iamniting@gmail.com>
2017-10-11 22:05:57 +0530

    Create Multiset compare.cpp

---
+++ C Plus Plus/STL/Multiset/Multiset compare.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+void showSet(multiset<int> s)
+{
+    multiset<int>::iterator it;
+    for(it = s.begin(); it != s.end(); it++)
+        cout << *it << " ";
+        cout << endl;
+}
+
+int main()
+{
+    multiset<int> s;
+    multiset<int> :: iterator itr;
+    multiset<int> :: key_compare cmp;
+    
+    for(int i=1; i<5; i++)
+        s.insert(i);
+    
+    cmp = s.key_comp();
+    cout << "set contains" << endl;
+    showSet(s);
+    itr = s.begin();
+    while( cmp( *(itr), 4) )
+    {
+        cout << *itr << " ";
+        itr++;
+    }
+    return 0;
+}
