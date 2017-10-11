1ac2fdff479b81a585e1cd2a9dcf0e419ddbb7f4
Nitin Goyal <iamniting@gmail.com>
2017-10-11 19:26:14 +0530

    Update Set count.cpp

--- C Plus Plus/STL/Set/Set count.cpp
+++ C Plus Plus/STL/Set/Set count.cpp
@@ -12,18 +12,18 @@ void showSet(set<int> s)
 
 int main()
 {
-	set<int> s;
-	s.insert(22);
-	s.insert(14);
-	s.insert(16);
-	s.insert(17);
-	s.insert(16);
-	s.insert(11);
-	
-	cout << "set contains" << endl;
-	showSet(s);
-	
+    set<int> s;
+    s.insert(22);
+    s.insert(14);
+    s.insert(16);
+    s.insert(17);
+    s.insert(16);
+    s.insert(11);
+    
+    cout << "set contains" << endl;
+    showSet(s);
+    
     cout << "s.count(2) " << s.count(2) << endl;
     cout << "s.count(22) " << s.count(22) << endl;
-	return 0;
+    return 0;
 }
