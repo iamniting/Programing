6bd89eeb2a00f6073e3e77d6478613f73ca6a652
Nitin Goyal <iamniting@gmail.com>
2017-10-11 21:56:21 +0530

    Create Multiset basics.cpp

---
+++ C Plus Plus/STL/Multiset/Multiset basics.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+void showSet(multiset<int> s)
+{
+    multiset<int>::iterator it;
+    for(it = s.begin(); it != s.end(); it++)
+        cout << *it << " ";
+        cout << endl;
+}
+
+int main()
+{
+    multiset<int> s;
+    s.insert(22);
+    s.insert(14);
+    s.insert(16);
+    s.insert(17);
+    s.insert(16);
+    s.insert(11);
+    
+    cout << "set contains" << endl;
+    showSet(s);
+    
+    cout << "s.begin() " << *s.begin() << endl;
+    cout << "*s.begin()+1 " << *s.begin()+1 << endl;
+    
+    cout << "*s.rbegin() " << *s.rbegin() <<endl;
+    cout << "*s.rbegin()+1 " << *s.rbegin()+1 << endl;
+    
+    cout << "s.size() " << s.size() << endl;
+    cout << "s.empty() " << s.empty() << endl;
+    s.clear();
+    return 0;
+}
