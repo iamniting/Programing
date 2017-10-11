891181f317d16725f0b6858bff04e8520a9d8fa9
Nitin Goyal <iamniting@gmail.com>
2017-10-11 19:21:08 +0530

    Update Set basics.cpp

--- C Plus Plus/STL/Set/Set basics.cpp
+++ C Plus Plus/STL/Set/Set basics.cpp
@@ -12,25 +12,25 @@ void showSet(set<int> s)
 
 int main()
 {
-	set<int> s;
-	s.insert(22);
-	s.insert(14);
-	s.insert(16);
-	s.insert(17);
-	s.insert(16);
-	s.insert(11);
-	
-	cout << "set contains" << endl;
-	showSet(s);
-	
+    set<int> s;
+    s.insert(22);
+    s.insert(14);
+    s.insert(16);
+    s.insert(17);
+    s.insert(16);
+    s.insert(11);
+    
+    cout << "set contains" << endl;
+    showSet(s);
+    
     cout << "s.begin() " << *s.begin() << endl;
     cout << "*s.begin()+1 " << *s.begin()+1 << endl;
     
     cout << "*s.rbegin() " << *s.rbegin() <<endl;
     cout << "*s.rbegin()+1 " << *s.rbegin()+1 << endl;
     
-	cout << "s.size() " << s.size() << endl;
-	cout << "s.empty() " << s.empty() << endl;
-	s.clear();
-	return 0;
+    cout << "s.size() " << s.size() << endl;
+    cout << "s.empty() " << s.empty() << endl;
+    s.clear();
+    return 0;
 }
